ccbb9b018c16c4e61719fe3317f8a40f94397b82
Dipangshu Roy <droy2ju@gmail.com>
2022-09-10 01:12:15 +0530

    init SizeConfig to use it

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sample_app/config/size_config.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context); // initialize SizeConfig with context
+
     return Scaffold(
       appBar: AppBar(
         title: Text(toString()),
