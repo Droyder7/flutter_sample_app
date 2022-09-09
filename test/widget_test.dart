2ba9a5d9ade14028dc143129156b9d9dd45c0ccc
Dipangshu Roy <droy2ju@gmail.com>
2022-09-09 23:45:41 +0530

    cleaned app with empty homepage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_sample_app/main.dart';
 
 void main() {
+  /* 
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
@@ -26,5 +27,13 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  }); 
+  */
+
+  testWidgets('My App smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('1'), findsNothing);
   });
 }
